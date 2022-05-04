6864fe83bb710688cdd0d3f4f6a107920e8fa9ff
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-05-03 19:34:49 -0500

    work in progress

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='/' className='link footer__link'>
+      Created By DeQuavion Wilburn
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,49 @@
+const header = {
+  homepage: 'https://github.com/quawilburn',
+  title: 'DW',
+}
+
+const about = {
+  name: 'DeQuavion Wilburn',
+  role: 'Software Engineer',
+  description:
+    'I am a passionate and dedicated full stack developer. I started my coding journey with BloomTech Institute of Technnology and I feel that was the greatest decision of my life so far. Coding has given me a career and hobby I will always love.',
+  resume:
+    'https://docs.google.com/document/d/1DVK6BQm3qjc_6DCAaiKKsCPwve81f9GRJyNhfkwy1q8/edit?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/dequavionwilburn',
+    github: 'https://github.com/quawilburn',
+  },
+}
+
+const projects = [
+  {
+    name: 'Book Finder',
+    description:
+      'This project uses Googles book API to find books based on the users search.',
+    stack: ['HTML', 'CSS', 'Bootstrap', 'Javascript'],
+    sourceCode: 'https://github.com/quawilburn/BookFinder',
+    livePreview: 'https://book-finder-wheat.vercel.app/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Express',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'PostreSQL',
+  'Jest',
+  'NodeJs',
+]
+
+const contact = {
+  email: 'qua.thesage19@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
